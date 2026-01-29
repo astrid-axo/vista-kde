cc4488cc38ae76999b594a689db03a8b1ffbf631
astrid-axo <git2@bork.quest>
2026-01-29 07:16:12 -0500

    fix libplasma patch

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,21 +13,18 @@ install_component () {
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
 # this installs a package from fedora repos
 dnf -y install ninja plasma-workspace-devel unzip kvantum qt6-qtmultimedia-devel qt6-qt5compat-devel libplasma-devel qt6-qtbase-devel qt6-qtwayland-devel plasma-activities-devel kf6-kpackage-devel kf6-kglobalaccel-devel qt6-qtsvg-devel wayland-devel plasma-wayland-protocols kf6-ksvg-devel kf6-kcrash-devel kf6-kguiaddons-devel kf6-kcmutils-devel kf6-kio-devel kdecoration-devel kf6-ki18n-devel kf6-knotifications-devel kf6-kirigami-devel kf6-kiconthemes-devel cmake gmp-ecm-devel kf5-plasma-devel libepoxy-devel kwin-devel kf6-karchive kf6-karchive-devel plasma-wayland-protocols-devel qt6-qtbase-private-devel qt6-qtbase-devel kf6-knewstuff-devel kf6-knotifyconfig-devel kf6-attica-devel kf6-krunner-devel kf6-kdbusaddons-devel kf6-sonnet-devel plasma5support-devel plasma-activities-stats-devel polkit-qt6-1-devel qt-devel libdrm-devel kf6-kitemmodels-devel kf6-kstatusnotifieritem-devel layer-shell-qt-devel
 
 # fonts
 REACTXP="https://github.com/microsoft/reactxp/raw/refs/heads/master/samples/TodoList/src/resources/fonts"
-curl -Ls -o /usr/share/fonts/SegoeUI-Regular.ttf $REACTXP/SegoeUI-Regular.ttf
-curl -Ls -o /usr/share/fonts/SegoeUI-Semibold.ttf $REACTXP/SegoeUI-Semibold.ttf
-curl -Ls -o /usr/share/fonts/SegoeUI-Bold.ttf $REACTXP/SegoeUI-Bold.ttf
+# curl -O https://github.com/mrbvrz/segoe-ui-linux/archive/refs/heads/master.zip
 curl -Ls -o /usr/share/fonts/Lucida-Console.ttf https://github.com/elliotwoods/kimchiandchips/raw/refs/heads/master/oF/apps%20VS/PC%20Encode%203.0/bin/data/Lucida%20Console.ttf
 
 fc-cache -f -r -v
 
-git clone https://gitgud.io/catpswin56/vistathemeplasma /tmp/vistathemeplasma
-
+git clone --depth 1 https://gitgud.io/catpswin56/vistathemeplasma /tmp/vistathemeplasma
+# cp -r /ctx/VistaThemePlasma /tmp/vistathemeplasma
 cd /tmp/vistathemeplasma
 CUR="$(pwd)"
 
@@ -94,7 +91,66 @@ cp -r $CUR/misc/kvantum/Kvantum /etc
 echo -e "[General]\ntheme=WindowsVistaKvantum_Aero" > /usr/share/Kvantum/kvantum.kvconfig
 
 cd $CUR/misc/libplasma
-sh install.sh --ninja
+
+VERSION="6.5.5"
+URL="https://invent.kde.org/plasma/libplasma/-/archive/v${VERSION}/libplasma-v${VERSION}.tar.gz"
+ARCHIVE="libplasma-v${VERSION}.tar.gz"
+SRCDIR="libplasma-v${VERSION}"
+
+INSTALLDST="/usr/lib/x86_64-linux-gnu/qt6/qml/org/kde/plasma/core/libcorebindingsplugin.so"
+LIBDIR="/usr/lib/x86_64-linux-gnu/"
+
+if [ ! -d ${LIBDIR} ]; then
+	LIBDIR="/usr/lib64/"
+fi
+
+if [ ! -f ${INSTALLDST} ]; then
+	INSTALLDST="/usr/lib64/qt6/qml/org/kde/plasma/core/libcorebindingsplugin.so"
+fi
+
+if [ ! -d ./build/${SRCDIR} ]; then
+	rm -rf build
+	mkdir -p build
+	echo "Downloading $ARCHIVE"
+	curl $URL -o ./build/$ARCHIVE
+	tar -xvf ./build/$ARCHIVE -C ./build/
+	echo "Extracted $ARCHIVE"
+fi
+
+cp -r src ./build/$SRCDIR/
+cd ./build/$SRCDIR/
+mkdir -p build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=/usr .. -G Ninja
+cmake --build . --target corebindingsplugin
+
+TMPDIR="/tmp/kfiles"
+mkdir -p $TMPDIR
+cp ./bin/org/kde/plasma/core/libcorebindingsplugin.so $TMPDIR
+for filename in "$PWD/bin/libPlasma"*; do
+	echo "Copying $filename to $TMPDIR"
+    cp "$filename" "$TMPDIR"
+done
+
+if [ ! -d ${LIBDIR} ]; then
+	LIBDIR="/usr/lib64/"
+fi
+
+if [ ! -f ${INSTALLDST} ]; then
+	INSTALLDST="/usr/lib64/qt6/qml/org/kde/plasma/core/libcorebindingsplugin.so"
+fi
+
+cp "$TMPDIR/libcorebindingsplugin.so" $INSTALLDST
+
+for filename in "$TMPDIR/libPlasma"*; do
+	echo "Copying $filename to $LIBDIR"
+	cp "$filename" "$LIBDIR"
+done
+
+echo "Done."
+
+rm -r $TMPDIR
+
 # cd $CUR/misc/uac-polkitagent
 # sh install.sh --ninja
 # sh add_rule.sh --ninja
@@ -126,7 +182,7 @@ cd PlymouthVista
 chmod +x ./compile.sh
 chmod +x ./install.sh
 ./compile.sh
-echo "1" | ./install.sh
+./install.sh -o -s -q
 
 
 # Use a COPR Example:
